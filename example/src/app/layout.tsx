6409226230864d4b7db07ede4a401b9fe0d26b75
tonyedgal <tonyedgal@gmail.com>
2025-08-13 18:31:23 +0100

    feat: reduce number of client components

--- example/src/app/layout.tsx
+++ example/src/app/layout.tsx
@@ -2,10 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '../components/theme/theme-provider';
-import { SpacemanThemeProvider } from '../components/SpacemanThemeProvider';
 import BackgroundPattern from '../components/BackgroundPattern';
 import NavBar from '@/components/NavBar';
-// import CrossBackground from '../components/CrossBackground';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,27 +25,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} ${geistMono.variable} antialiased overflow-hidden`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          <SpacemanThemeProvider
-            themes={['light', 'dark', 'system']}
-            colorThemes={['default', 'blue', 'green', 'purple', 'caffeine', 'mono', 'supabase']}
-            defaultTheme="system"
-            defaultColorTheme="default"
-          >
-            <NavBar />
-            <main className="relative min-h-screen w-full">
-              <div className="relative grid min-h-screen grid-cols-[2.5rem_auto_2.5rem]">
-                <div className="relative col-start-2 h-full w-full">
-                  <BackgroundPattern />
-                  <div className="gap-6 w-full">{children}</div>
-                </div>
-
-                <div className="relative -right-px col-start-1 row-span-full row-start-1 border-x border-border bg-[image:repeating-linear-gradient(315deg,var(--border)_0,var(--border)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px]"></div>
-                <div className="relative -left-px col-start-3 row-span-full row-start-1 border-x border-border bg-[image:repeating-linear-gradient(315deg,var(--border)_0,var(--border)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px]"></div>
-                <div className="relative -bottom-px col-span-full col-start-1 row-start-2 h-px bg-border"></div>
-                <div className="relative -top-px col-span-full col-start-1 row-start-4 h-px bg-border"></div>
+          <NavBar />
+          <main className="relative min-h-screen w-full">
+            <div className="relative grid min-h-screen grid-cols-[2.5rem_auto_2.5rem]">
+              <div className="relative col-start-2 h-full w-full">
+                <BackgroundPattern />
+                <div className="gap-6 w-full">{children}</div>
               </div>
-            </main>
-          </SpacemanThemeProvider>
+
+              <div className="relative -right-px col-start-1 row-span-full row-start-1 border-x border-border bg-[image:repeating-linear-gradient(315deg,var(--border)_0,var(--border)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px]"></div>
+              <div className="relative -left-px col-start-3 row-span-full row-start-1 border-x border-border bg-[image:repeating-linear-gradient(315deg,var(--border)_0,var(--border)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px]"></div>
+              <div className="relative -bottom-px col-span-full col-start-1 row-start-2 h-px bg-border"></div>
+              <div className="relative -top-px col-span-full col-start-1 row-start-4 h-px bg-border"></div>
+            </div>
+          </main>
         </ThemeProvider>
       </body>
     </html>
