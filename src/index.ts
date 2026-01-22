9ba1f28d3bdd3d583f65a4c7e198a9aa4cd3f62b
tonyedgal <tonyedgal@gmail.com>
2026-01-22 01:51:15 +0100

    feat: add next provider with ssr features

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ export { useThemeAnimation } from './hooks/use-theme-animation'
 
 export { ThemeSwitcher } from './components/ThemeSwitcher'
 export { ThemeSelector } from './components/ThemeSelector'
+
 export { SpacemanThemeProvider, useSpacemanTheme } from './components/SpacemanThemeProvider'
+
+// Framework-specific optimized providers
+export { NextThemeProvider, useNextTheme } from './components/NextThemeProvider'
+export { TanStackThemeProvider, useTanStackTheme } from './components/TanStackThemeProvider'
 export { ViteThemeProvider, useViteTheme } from './components/ViteThemeProvider'
 
 export type {
